74c75bf8b0821373e42a4610034f159e87fbef6e
liuning <liuning@ifeidao.com>
2019-08-22 14:13:55 +0800

    添加依赖

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //这是一个SpringBoot的项目
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+}
